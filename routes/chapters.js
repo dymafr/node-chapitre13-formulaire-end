cef130610cd607cdf9fc8bce555db5165321c359
dymafr <contact@dyma.fr>
2021-03-29 20:32:31 +0200

    maj

--- routes/chapters.js
+++ routes/chapters.js
@@ -4,22 +4,34 @@ const util = require('util');
 
 router.post('/', (req, res) => {
   const body = req.body;
-  console.log({ body });
+  console.log(body);
   const newChapter = new Chapters({
     ...body,
-    active: body.active ? true : false
+    active: body.active ? true : false,
   });
 
-  console.log({ newChapter });
+  console.log(newChapter);
 
-  newChapter.save().then( chapter =>  {
-    console.log({ chapter });
-    res.redirect('/');
-  }).catch( err => {
-    const errors = Object.keys(err.errors).map( key => err.errors[key].message );
-    console.log(util.inspect(errors, { compact: true, depth: 5, breakLength: 80, colors: true }));
-    res.status(400).render('index', { errors });
-  })
-})
+  newChapter
+    .save()
+    .then((chapter) => {
+      console.log(chapter);
+      res.redirect('/');
+    })
+    .catch((err) => {
+      const errors = Object.keys(err.errors).map(
+        (key) => err.errors[key].message
+      );
+      console.log(
+        util.inspect(errors, {
+          compact: true,
+          depth: 5,
+          breakLength: 80,
+          colors: true,
+        })
+      );
+      res.status(400).render('index', { errors });
+    });
+});
 
 module.exports = router;
